627af2bcaaa1e5f1a5f24643db1d945b8a69c6c6
achraf.zouchou <achraf.zouchou@gmail.com>
2025-12-19 11:23:25 +0100

    Update dependencies and enhance file upload functionality
    - Added new dependencies: `core-foundation`, `system-configuration`, `read-progress-stream`, `tauri-plugin-upload`, and `windows-registry` to improve application capabilities.
    - Updated `Cargo.toml` to include `tauri-plugin-upload` for enhanced file upload support.
    - Modified file upload handling in `server.rs` to detect APK files and set the correct MIME type based on file content.
    - Adjusted UI settings in `tauri.conf.json` to improve layout.
    - Updated capabilities in `default.json` to include upload permissions.
    
    These changes enhance the application's file handling capabilities and improve user experience during uploads.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -285,6 +285,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_upload::init())
         .setup(|app| {
             let config = load_config(app.handle());
             let port = config.port;
